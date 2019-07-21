3148ed3b872a5bf7e8ddcf2e010750d1248e4f85
Charlie Chung <charlieschung@gmail.com>
2019-07-21 13:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // add solution here
+let emptyArray = [];
+function theBeatlesPlay(musicians, instruments){
+  let output;
+  for (let i=0; i<musicians.length;i++){
+    emptyArray.push(output = musicians[i] + " plays " + instruments[i]);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(facts){
+  let newFacts = [];
+  let i=0;
+  while(i<facts.length){
+    newFacts.push(facts[i]+"!!!");
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(n){
+  let emptyArray;
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
